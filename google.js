6d70a854a221d3965f8dace5f0f3560e2843125e
Box <40639989+theBoxed@users.noreply.github.com>
2020-11-10 23:06:04 -0700

    small tweaks

--- google.js
+++ google.js
@@ -1,8 +1,5 @@
 const { GoogleSpreadsheet } = require('google-spreadsheet');
-const { GOOGLE_PRIVATE_KEY,GOOGLE_SERVICE_ACCOUNT_EMAIL } = require('./config');
 const creds = require('./client_secret.json');
-// const stor = require('./routes/storage.js')
-
 const doc = new GoogleSpreadsheet('1VcTA1paspqCAswKzS3uUStiEOxMOsLIrBub4WZ6FinI');
 
 
@@ -57,41 +54,6 @@ for(let i = 1; i<13; i++){
 module.exports = updateGoogleSheet;
 
 
-
-// (async() => {
-
-// await doc.useServiceAccountAuth(creds)
-// await doc.loadInfo(); // loads document properties and worksheets
-// // console.log(doc);
-// await doc.updateProperties({ title: 'renamed doc' });
-
-// const top6Sheet = doc.sheetsById[sheetIds['top6']];
-// const winSheet = doc.sheetsById[sheetIds['wins']];
-// const pointsScored = doc.sheetsById[sheetIds['pointsScored']];
-// const top6SheetRows = await top6Sheet.getRows();
-// const winSheetRows = await winSheet.getRows();
-// const pointsScoredRows = await pointsScored.getRows();
-
-// //loop through owners
-// for(let i = 1; i<13; i++){
-//     let winRow = winSheetRows[i-1];
-//     let top6Row = top6SheetRows[i-1];
-//     let pointScoredRow = pointsScoredRows[i-1];
-//     let team = stor[i.toString()]['weeks'];
-
-//     for(let j=1; j<=13; j++) {
-//         let rowHeader = ('Week' + ' ' + j);
-//         winRow[rowHeader] = team[j.toString()]['wins']
-//         top6Row[rowHeader] = team[j.toString()]['top6']
-//         pointScoredRow[rowHeader] = team[j.toString()]['pointsScored']
-//     }
-//     await winRow.save();
-//         await pointScoredRow.save();
-//             await top6Row.save();
-
-// }
-
-// })();
 
 
 
